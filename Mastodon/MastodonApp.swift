97304057ea948ea2bd97c4290092dc20e2fead21
Nathan Wale <nathan.wale@icloud.com>
2023-09-07 10:19:23 +1000

    Initial Commit

---
+++ Mastodon/MastodonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastodonApp.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 7/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct MastodonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
