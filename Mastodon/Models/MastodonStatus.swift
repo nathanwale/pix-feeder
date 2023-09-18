e8e622eed6392e100f5630aef59ed03bb56c57cc
Nathan Wale <nathan.wale@icloud.com>
2023-09-18 11:27:10 +1000

    Initial commit
    JSON Decodable model to represent Mastodon Statuses

---
+++ Mastodon/Models/MastodonStatus.swift
@@ -0,0 +1,172 @@
+//
+//  MastodonStatus.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 7/9/2023.
+//
+
+import Foundation
+
+class MastodonStatus
+{
+    /// Status ID
+    var id: Identifier!
+    
+    /// URI of the status used for federation
+    var uri: URL!
+    
+    /// The date this status was created
+    var createdAt: Date!
+    
+    /// The MastodonAccount object that created this status
+    var account: MastodonAccount!
+    
+    /// Content of status as HTML
+    var content: String!
+    
+    /// The Visibility of this Status
+    var visibility: StatusVisibility!
+    
+    /// Is this status marked as sensitive content?
+    var sensitive: Bool!
+    
+    /// Description of content hidden behind spoiler/content warning
+    var spoilerText: String!
+    
+    /// Media attached to this Status
+    var mediaAttachments: MastodonMediaAttachment!
+    
+    /// Application a status was posted with
+    var application: Application?
+    
+    /// Mentioned users in this Status
+    var mentions: [Mention]!
+    
+    /// Tags attached to this Status
+    var tags: [Tag]!
+    
+    /// Custom emojis used in this Status
+    var emojis: [MastodonCustomEmoji]!
+    
+    /// Number of boosts this Status has received
+    var reblogsCount: Int!
+    
+    /// Number of Favourites this Status has received
+    var favouritesCount: Int!
+    
+    /// Number of replies this Status has received
+    var repliesCount: Int!
+    
+    /// Location of the Statuses HTML representation. Optional
+    var url: URL?
+    
+    /// Status ID this status is replying to. Optional
+    var inReplyToId: Identifier?
+    
+    /// Account ID this status is replying to. Optional
+    var inReplyToAccountId: MastodonAccountId?
+    
+    /// Status being reblogged. Optional
+    var reblog: MastodonStatus?
+    
+    /// A Poll if attached. Otherwise `nil`
+    var poll: MastodonPoll?
+    
+    /// Preview card for linked content. Optional
+    var card: MastodonPreviewCard?
+    
+    /// Language of Status as ISO two-letter code. Optional
+    var language: String?
+    
+    /// Plain text source of status. Optional
+    var text: String?
+    
+    /// When the status was last edited. `nil` if not edited
+    var editedAt: Date?
+    
+    /// Has the current user faved this Status? Optional
+    var favourited: Bool?
+    
+    /// Has the current user reblogged this Status? Optional
+    var reblogged: Bool?
+    
+    /// Has the current user muted notifications for this Status? Optional
+    var muted: Bool?
+    
+    /// Has the current user bookmarked this Status? Optional
+    var bookmarked: Bool?
+    
+    /// Has the current user pinned this Status? Optional
+    var pinned: Bool?
+    
+    /// Tthe current user's filter and keywords for this Status. Optional
+//    - filtered *optional*
+}
+
+    
+// MARK: - inner types
+extension MastodonStatus
+{
+    typealias Identifier = String
+    
+    ///
+    /// Application a status was posted with
+    ///
+    struct Application: Codable
+    {
+        var name: String
+        var website: URL?
+    }
+    
+    ///
+    /// The visibility of a Mastodon Status
+    ///
+    enum StatusVisibility: String, Codable
+    {
+        /// Visible to everyone, shown in public timelines
+        case `public` = "public"
+        
+        /// Visible to public, but not included in public timelines
+        case unlisted = "unlisted"
+        
+        /// Visible to followers only, and to any mentioned users
+        case `private` = "private"
+        
+        ///  Visible only to mentioned users
+        case direct = "direct"
+    }
+    
+    ///
+    /// Mentioned User in a Status
+    ///
+    struct Mention
+    {
+        /// Account ID of the mentioned user
+        var id: MastodonAccountId
+        
+        /// Username of the mentioned user
+        var username: String
+        
+        /// Location of mentioned user's profile
+        var url: URL
+        
+        /// Webfinger account of mentioned user.
+        /// `username` for local users, otherwise `username@domain`
+        var acct: WebfingerAccount
+    }
+    
+    ///
+    /// Tag attached to a Status
+    ///
+    struct Tag
+    {
+        /// Value of hashtag after `#` sign
+        var name: String
+        
+        /// A link to this hashtag on the instance
+        var url: URL
+    }
+}
+
+
+
