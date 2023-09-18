7e4049e06f6144e67a30c2adf6aa111204d47595
Nathan Wale <nathan.wale@icloud.com>
2023-09-18 17:06:10 +1000

    Initial commit
    Tests for MastodonCustomEmojiTests, and JSON test data

---
+++ MastodonTests/MastodonCustomEmojiTests.swift
@@ -0,0 +1,25 @@
+//
+//  MastodonAccountTests.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 8/9/2023.
+//
+
+import XCTest
+
+@testable import Mastodon
+
+final class MastodonCustomEmojiTests: XCTestCase
+{
+    func testDecodeSingleCustomEmojiFromJson() throws
+    {
+        let fileUrl = Bundle(for: Self.self).url(forResource: "single-custom-emoji", withExtension: "json")!
+        let emoji: MastodonCustomEmoji = JsonLoader.fromLocalUrl(fileUrl)
+        
+        XCTAssertEqual(emoji.shortcode, "blobaww")
+        XCTAssertEqual(emoji.url, URL(string: "https://files.mastodon.social/custom_emojis/images/000/011/739/original/blobaww.png"))
+        XCTAssertEqual(emoji.staticUrl, URL(string: "https://files.mastodon.social/custom_emojis/images/000/011/739/static/blobaww.png"))
+        XCTAssertEqual(emoji.visibleInPicker, true)
+        XCTAssertEqual(emoji.category, "Blobs")
+    }
+}
