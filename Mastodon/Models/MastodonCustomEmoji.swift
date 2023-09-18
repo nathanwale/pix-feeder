595fce9e0530ecd587b164bbbe36f700eed740a2
Nathan Wale <nathan.wale@icloud.com>
2023-09-18 11:24:16 +1000

    Initial commit
    JSON Decodable model to represent Custom Emoji

---
+++ Mastodon/Models/MastodonCustomEmoji.swift
@@ -0,0 +1,30 @@
+//
+//  MastodonCustomEmoji.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 7/9/2023.
+//
+
+import Foundation
+
+/**
+    A custom emoji
+        - Decoded from API as described at: https://docs.joinmastodon.org/entities/CustomEmoji/
+ */
+struct MastodonCustomEmoji: Codable, Equatable
+{
+    /// The name of the emoji as referenced in posts
+    let shortcode: String?
+    
+    /// Location of emoji image (may be animated)
+    let url: URL
+    
+    /// Location of non-animated version of emoji image
+    let staticUrl: URL
+    
+    /// Is this emoji visible in the picker? Otherwise it's unlisted
+    let visibleInPicker: Bool?
+    
+    /// Category of emoji for organising in picker
+    let category: String?
+}
