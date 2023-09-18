171c00dbed5986756ae2aa4fd2fe9bcbae416756
Nathan Wale <nathan.wale@icloud.com>
2023-09-18 17:07:04 +1000

    Filling out tests for MastodonPoll

--- MastodonTests/MastodonPollTests.swift
+++ MastodonTests/MastodonPollTests.swift
@@ -15,6 +15,22 @@ final class MastodonPollTests: XCTestCase
     {
         let fileUrl = Bundle(for: Self.self).url(forResource: "single-poll", withExtension: "json")!
         let poll: MastodonPoll = JsonLoader.fromLocalUrl(fileUrl)
+        
         XCTAssertEqual(poll.id, "34830")
+        XCTAssertEqual(poll.expiresAt!.description, "2019-12-05 04:05:08 +0000")
+        XCTAssertEqual(poll.expired, true)
+        XCTAssertEqual(poll.multiple, false)
+        XCTAssertEqual(poll.votesCount, 10)
+        XCTAssertEqual(poll.votersCount, nil)
+        XCTAssertEqual(poll.voted, true)
+        XCTAssertEqual(poll.ownVotes, [1])
+        XCTAssertEqual(poll.emojis, [])
+        
+        let options = [
+            MastodonPoll.Options(title: "accept", votesCount: 6),
+            MastodonPoll.Options(title: "deny", votesCount: 4),
+        ]
+        
+        XCTAssertEqual(poll.options, options)
     }
 }
