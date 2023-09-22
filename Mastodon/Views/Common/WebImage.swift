75f8a874c6ad51d0cef44742b2f16d646ce4f958
Nathan Wale <nathan.wale@icloud.com>
2023-09-22 16:21:34 +1000

    WebImage: Cached image from URL
    Uses AsyncCachedImage
    + scaledToFit by default
    + includes ProgressView for loading image

---
+++ Mastodon/Views/Common/WebImage.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileImage.swift
+//  Mastodon
+//
+//  Created by Nathan Wale on 19/9/2023.
+//
+
+import SwiftUI
+import CachedAsyncImage
+
+struct WebImage: View
+{
+    var url: URL?
+    
+    var body: some View
+    {
+        CachedAsyncImage(url: url) {
+            $0.resizable()
+        } placeholder: {
+            ProgressView()
+        }
+        .scaledToFit()
+    }
+}
+
+struct WebImage_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        let cachedUrl = URL(string: "https://files.mastodon.social/accounts/avatars/110/528/637/375/951/012/original/2d14c64b7a9e1f10.jpeg")!
+        let freshUrl = URL(string: "https://picsum.photos/1000")!
+        HStack {
+            VStack {
+                WebImage(url: cachedUrl)
+                Text("Cached")
+            }
+            VStack {
+                WebImage(url: freshUrl)
+                Text("Fresh")
+            }
+        }
+    }
+}
