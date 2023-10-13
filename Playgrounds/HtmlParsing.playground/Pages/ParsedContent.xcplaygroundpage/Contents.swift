09d4f69c5fff3e3ebc0993b075cf783c4ec18db1
Nathan Wale <nathan.wale@icloud.com>
2023-10-13 21:05:21 +1100

    Isolating h-card parsing bug in ParsedStatusContent

---
+++ Playgrounds/HtmlParsing.playground/Pages/ParsedContent.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let idWithHCard = "110693860790938673"
+let status = MastodonStatus.previews.first { $0.id == idWithHCard }!
+let content = (status.reblog ?? status).content
+
+let parsedContent = ParsedStatusContent(html: content!)
+parsedContent.tokens
+parsedContent.attributedString
