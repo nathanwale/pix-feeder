2b37499039a4547f3157160a876f93b94c032ea2
Nathan Wale <nathan.wale@icloud.com>
2023-10-26 19:55:16 +1100

    Indicators for prior comments and replies, auto scrolls to status being viewed

--- Mastodon/Views/Status/StatusDetail.swift
+++ Mastodon/Views/Status/StatusDetail.swift
@@ -11,6 +11,7 @@ struct StatusDetail: View
 {
     let status: MastodonStatus
     let context: MastodonStatus.Context?
+    @Namespace var currentStatusId
     
     var ancestors: [MastodonStatus]
     {
@@ -30,18 +31,43 @@ struct StatusDetail: View
         
     var body: some View 
     {
-        ScrollView
+        ScrollViewReader
         {
-            VStack
+            scrollProxy in
+            ScrollView
             {
-                statusList(ancestors)
-                StatusPost(status)
-                    .background(Color.accentColor.opacity(0.1))
-                Text("Replies").font(.headline)
-                statusList(descendants)
+                VStack
+                {
+                    statusList(ancestors)
+                    indicator("Prior", icon: .chevronUp)
+                    StatusPost(status)
+                        .background(Color.accentColor.opacity(0.1))
+                        .id(currentStatusId)
+                        .padding(0)
+                    indicator("Replies", icon: .chevronDown)
+                    statusList(descendants)
+                }
+                .navigationTitle("Post by \(status.account.displayName)")
+                .onAppear {
+                    scrollProxy.scrollTo(currentStatusId, anchor: .top)
+                }
             }
-            .navigationTitle("Post by \(status.account.displayName)")
         }
+    }
+    
+    func indicator(_ text: String, icon: Icon) -> some View
+    {
+        HStack
+        {
+            Spacer()
+            icon.image
+            Text(text).font(.headline)
+                .padding(5)
+            icon.image
+            Spacer()
+        }
+        .foregroundColor(.white)
+        .background(Color.secondary)
     }
     
     func statusList(_ statuses: [MastodonStatus]) -> some View
@@ -62,5 +88,4 @@ struct StatusDetail: View
         MastodonStatus.preview,
         context: MastodonStatus.previewContext
     )
-    .padding(20)
 }
