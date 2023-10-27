b4dac0cab64f0b75ddac920d04d324bc4b80bad9
Nathan Wale <nathan.wale@icloud.com>
2023-10-27 12:06:23 +1100

    Docs

--- Mastodon/Models/AppNavigation.swift
+++ Mastodon/Models/AppNavigation.swift
@@ -7,27 +7,36 @@
 
 import Foundation
 
+///
+/// Represents the state of navigation in the App.
+/// Navigation can be persisted across sessions.
+///
 final class AppNavigation: ObservableObject, Codable
 {
+    /// The navigation path as a list of Statuses
     @Published var path: [MastodonStatus]
     
+    /// Coding keys for persistence
     enum CodingKeys: String, CodingKey
     {
         case navigationPath
     }
     
+    /// Init with empty path
     init()
     {
         path = []
     }
         
-    required init(from decoder: Decoder) throws 
+    /// Decode from persisted object
+    required init(from decoder: Decoder) throws
     {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         path = try container.decode([MastodonStatus].self, forKey: CodingKeys.navigationPath)
     }
     
-    func encode(to encoder: Encoder) throws 
+    /// Encode into persisted object
+    func encode(to encoder: Encoder) throws
     {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(path, forKey: CodingKeys.navigationPath)
