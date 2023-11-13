593f6960278cd4c134330ebffc9f1e37a3eef9e1
Nathan Wale <nathan.wale@icloud.com>
2023-11-13 11:03:52 +1100

    MastodonStatusRequest now handles `limit` and `timeFrame`

--- Mastodon/Remote/TimelineRequest.swift
+++ Mastodon/Remote/TimelineRequest.swift
@@ -7,34 +7,75 @@
 
 import Foundation
 
-protocol MastodonStatusRequest: ApiRequest where Response == [MastodonStatus] {}
-
-struct PublicTimelineRequest: MastodonStatusRequest
-{
-    let host: String
+enum ApiQueryTimeFrame {
+    case before(MastodonStatus)
+    case after(MastodonStatus)
     
-    let limit = 20
-    
-    let endpoint = "timelines/public"
-    
-    var queryItems: [URLQueryItem]? {
-        [URLQueryItem(name: "limit", value: "10")]
+    var queryItem: URLQueryItem {
+        switch self {
+            case .before(let status):
+                return .init(name: "since_id", value: status.id)
+            case .after(let status):
+                return .init(name: "min_id", value: status.id)
+        }
     }
 }
 
+///
+/// An API Request that returns MastodonStatuses
+///
+protocol MastodonStatusRequest: ApiRequest where Response == [MastodonStatus] 
+{
+    /// The host of the server. Eg.: "mastodon.social"
+    var host: String { get }
+    
+    /// Time frame for Statuses
+    var timeFrame: ApiQueryTimeFrame? { get set }
+}
 
+///
+/// Defaults for MastodonStatusRequest
+/// - limit: The max number of Statuses that will be returned
+///
+extension MastodonStatusRequest
+{
+    /// The max number of Statuses that will be returned
+    var limit: Int { 20 }
+    
+    var queryItems: [URLQueryItem]? {
+        var items = [
+            URLQueryItem(name: "limit", value: String(limit)),
+        ]
+        
+        if let timeFrame {
+            items.append(timeFrame.queryItem)
+        }
+        
+        return items
+    }
+}
+
+///
+/// A Public Timeline Request: will return public statuses that have just been posted
+///
+struct PublicTimelineRequest: MastodonStatusRequest
+{
+    let host: String
+    let endpoint = "timelines/public"
+    var timeFrame: ApiQueryTimeFrame?
+}
+
+///
+/// A Timeline Request for a particular account
+/// - userid: The Mastodon Account ID for the User
+///
 struct UserTimelineRequest: MastodonStatusRequest
 {
     let host: String
     let userid: MastodonAccountId
-    
-    let limit = 20
+    var timeFrame: ApiQueryTimeFrame?
     
     var endpoint: String {
         "accounts/\(userid)/statuses"
-    }
-    
-    var queryItems: [URLQueryItem]? {
-        [URLQueryItem(name: "limit", value: "10")]
     }
 }
