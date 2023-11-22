fd39474b7ed33ffb3abb78d71c112a934d52fb7e
Nathan Wale <nathan.wale@icloud.com>
2023-11-22 11:31:29 +1100

    MastodonStatus is now PixelfedStatus

--- Playgrounds/HtmlParsing.playground/Pages/ParsedContent.xcplaygroundpage/Contents.swift
+++ Playgrounds/HtmlParsing.playground/Pages/ParsedContent.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 let idWithHCard = "110693860790938673"
-let status = MastodonStatus.previews.first { $0.id == idWithHCard }!
+let status = PixelfedStatus.previews.first { $0.id == idWithHCard }!
 let content = (status.reblog ?? status).content
 
 let parsedContent = ParsedStatusContent(html: content!)
